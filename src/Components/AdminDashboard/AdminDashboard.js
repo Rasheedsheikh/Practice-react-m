8c8adaff27cbab075b8d42fc19145fe80d2925df
Karthick_Sivarajan <karthick.s@valuehealthsol.com>
2024-11-13 23:44:36 +0530

    assets & admin dashboard design

--- src/Components/AdminDashboard/AdminDashboard.js
+++ src/Components/AdminDashboard/AdminDashboard.js
@@ -1,8 +1,21 @@
+import { Button, Card } from 'antd'
 import React from 'react'
+import './AdminDashboard.css'
 
 const AdminDashboard = () => {
     return (
-        <div>AdminDashboard</div>
+        <>
+            <h2 className='contentHeading'>Dashboard</h2>
+            <div className='dashboardContents'>
+                <Card style={{ width: '400px' }}>
+                    <div className='headerAndImage'>
+                        <h3>Washing Machine</h3>
+                        <img src='/Assets/washing-machine.png' alt='washing machine' width={70} height={70} />
+                    </div>
+                    <Button>View</Button>
+                </Card>
+            </div>
+        </>
     )
 }
 
