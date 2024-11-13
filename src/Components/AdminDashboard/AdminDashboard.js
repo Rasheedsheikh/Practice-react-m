9686e00b2387d50efde85066940b71b150f07add
Karthick_Sivarajan <karthick.s@valuehealthsol.com>
2024-11-14 00:23:58 +0530

    Dashboard Design

--- src/Components/AdminDashboard/AdminDashboard.js
+++ src/Components/AdminDashboard/AdminDashboard.js
@@ -7,13 +7,50 @@ const AdminDashboard = () => {
         <>
             <h2 className='contentHeading'>Dashboard</h2>
             <div className='dashboardContents'>
-                <Card style={{ width: '400px' }}>
+                <Card className='dashboardContentCard'>
                     <div className='headerAndImage'>
                         <h3>Washing Machine</h3>
                         <img src='/Assets/washing-machine.png' alt='washing machine' width={70} height={70} />
                     </div>
-                    <Button>View</Button>
+                    <div className='orderListDiv'>
+                        <p>Total orders <b>14</b></p>
+                        <p>Pending orders <b>10</b></p>
+                    </div>
+                    <Button className='serviceViewBtn'>View</Button>
                 </Card>
+                <Card className='dashboardContentCard'>
+                    <div className='headerAndImage'>
+                        <h3>Refrigerator</h3>
+                        <img src='/Assets/smart-fridge.png' alt='smart-fridge' width={70} height={70} />
+                    </div>
+                    <div className='orderListDiv'>
+                        <p>Total orders <b>14</b></p>
+                        <p>Pending orders <b>10</b></p>
+                    </div>
+                    <Button className='serviceViewBtn'>View</Button>
+                </Card>
+                <Card className='dashboardContentCard'>
+                    <div className='headerAndImage'>
+                        <h3>Air Conditioner</h3>
+                        <img src='/Assets/air-conditioner.png' alt='air-conditioner' width={70} height={70} />
+                    </div>
+                    <div className='orderListDiv'>
+                        <p>Total orders <b>14</b></p>
+                        <p>Pending orders <b>10</b></p>
+                    </div>
+                    <Button className='serviceViewBtn'>View</Button>
+                </Card>
+                {/* <Card className='dashboardContentCard'>
+                    <div className='headerAndImage'>
+                        <h3>Air Conditioner</h3>
+                        <img src='/Assets/washing-machine.png' alt='washing machine' width={70} height={70} />
+                    </div>
+                    <div className='orderListDiv'>
+                    <p>Total orders <b>14</b></p>
+                        <p>Pending orders <b>10</b></p>
+                    </div>
+                    <Button className='serviceViewBtn'>View</Button>
+                </Card> */}
             </div>
         </>
     )
